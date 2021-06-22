613b3dbe158b0f3e0b28c5583c2e44fb94360bea
Santosh Gupta <39303159+santosh-gatech@users.noreply.github.com>
2021-06-22 09:43:01 +0000

    Update files

--- starter_file/endpoint.py
+++ starter_file/endpoint.py
@@ -3,7 +3,7 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://7fd14725-046b-4c61-9a70-87ef2bad6776.southcentralus.azurecontainer.io/score'
 
 # Two sets of data to score, so we get two results back
 
